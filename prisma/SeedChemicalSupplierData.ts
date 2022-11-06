b95efe02baec8a5d9acc3d90e7e61998fb0457a2
Doma Sucic <domagoj.sucic1@gmail.com>
2022-11-05 21:08:56 -0600

    Small fixes

--- prisma/SeedChemicalSupplierData.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export const chemicalSupplierData: Prisma.ChemicalSupplierCreateInput[] = [
-{ name: 'Contact', updatedBy: { connect: { email: 'dsucic@bonterraenergy.com' } }, createdBy: { connect: { email: 'dsucic@bonterraenergy.com' } } },
-{ name: 'Sterling', updatedBy: { connect: { email: 'dsucic@bonterraenergy.com' } }, createdBy: { connect: { email: 'dsucic@bonterraenergy.com' } } },
-{ name: 'CPS', updatedBy: { connect: { email: 'dsucic@bonterraenergy.com' } }, createdBy: { connect: { email: 'dsucic@bonterraenergy.com' } } }
-];
--- prisma/SeedCompanyData.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export const companytData: Prisma.CompanyCreateInput[] = [
-{ name: 'Western Pipeline', updatedBy: { connect: { email: 'dsucic@bonterraenergy.com' } }, createdBy: { connect: { email: 'dsucic@bonterraenergy.com' } } },
-{ name: 'Corrosion Technologies', updatedBy: { connect: { email: 'dsucic@bonterraenergy.com' } }, createdBy: { connect: { email: 'dsucic@bonterraenergy.com' } } },
-{ name: 'Allied Cathodic', updatedBy: { connect: { email: 'dsucic@bonterraenergy.com' } }, createdBy: { connect: { email: 'dsucic@bonterraenergy.com' } } }
-];
